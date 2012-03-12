d40986d9a998912e62ff6e15beb69cfb8c3b4e1f
Eddie Ridwan <eddie.ridwan@amc.org.au>
2012-03-12 13:21:48 +1100

    First commit of kabupaten.org

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kabupaten::Application.config.secret_token = '53b24c8c1399f692f09045e3a4c93344a161f5d5c8db4084e52a9a8d8cd09522bb7c295768f8c8fcfd41576722188eb93af2d57edc1cc20c9124a98df3af3503'
