0063a3659cbadebc3dde4ec4548b1d0923f2dcdc
Eddie Ridwan <eddie.ridwan@amc.org.au>
2012-07-01 20:26:36 +1000

    Add styles to project views.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
-
+    @projects = Project.order("projects.name_#{locale} asc").paginate(page: params[:page], per_page: 15)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @projects }
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :en_description, :en_name, :id_description, :id_name
+  attr_accessible :description_en, :name_en, :description_id, :name_id
   
   has_and_belongs_to_many :kabupatens, :join_table => 'project_kabupaten_mappings'
   has_and_belongs_to_many :contacts, :join_table => 'project_contact_mappings'
