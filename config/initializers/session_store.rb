bd84afcbfd3f4062e7b6bb43b26c545804f0ad02
Eddie Ridwan <eddie.ridwan@amc.org.au>
2012-07-07 22:52:44 +1000

    Set session_store to enable logins to work across all subdomains.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Kabupatenku::Application.config.session_store :cookie_store, key: '_kabupaten_session'
+# Set domain so that logins work across all subdomains (used to represent locales)
+# http://stackoverflow.com/questions/4203502/rails-3-no-local-session-cookie-with-domain-all
+Kabupatenku::Application.config.session_store :cookie_store, key: '_kabupaten_session', 
+  domain: "#{Rails.env == 'development' ? 'kabupaten.local' : 'kabupaten.com'}"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
