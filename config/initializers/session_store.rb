72fc1ccdefb703e7b765ad298022651a87c4e9a0
Eddie Ridwan <eddie.ridwan@amc.org.au>
2012-08-26 17:34:41 +1000

    Set session store back to default, as domain: setting does not appear to work.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
+Kabupatenku::Application.config.session_store :cookie_store, key: '_kabupaten_session'
+
 # Set domain so that logins work across all subdomains (used to represent locales)
 # http://stackoverflow.com/questions/4203502/rails-3-no-local-session-cookie-with-domain-all
-Kabupatenku::Application.config.session_store :cookie_store, key: '_kabupaten_session', 
-  domain: "#{Rails.env == 'development' ? 'kabupaten.local' : 'kabupaten.com'}"
+# Kabupatenku::Application.config.session_store :cookie_store, key: '_kabupaten_session', domain: {
+#   production: '.kabupaten.org',
+#   development: '.kabupaten.local'
+# }.fetch(Rails.env.to_sym, :all)
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
